f49456d53a5ee3f10d0a6181ff7d99004765b05c
Alex Nezhel <nejel@mail.ru>
2022-07-08 11:11:27 +0300

    Try to merge and execute this

---
+++ 4/python_is_cool.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+# Create instance
+parent = tk.Tk()
+
+lbl = tk.Label(parent, text="Привет")
+lbl.grid(column=0, row=0)
+
+# Add a title
+parent.title("Сообщение для Пушинки")
+
+# Start GUI
+parent.mainloop()
+
+# More about Tkinter
+# https://pythonru.com/uroki/obuchenie-python-gui-uroki-po-tkinter
